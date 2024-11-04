4def1a4617fb01f8dc290551a2926f3c9e0a1215
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-04 11:18:18 +0900

    Update Node.js version to 22 in deploy.yml

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-      <h1>About Page</h1>
-    </div>
-  </template>
-  
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView',  // 여러 단어로 구성된 이름 사용
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  </template>
-  
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',  // 여러 단어로 구성된 이름 사용
+}
+</script>
