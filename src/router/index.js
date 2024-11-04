2c5ec9bef57333aec80c69c0a2337045af2a392d
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-04 16:57:57 +0900

    update favicon

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import About from '../views/About.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-];
+  { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'About', component: About }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
 
-export default router;
+export default router
