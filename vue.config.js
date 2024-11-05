6fc36c356650d304e785d7ae22060f00f4ebe723
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-05 15:21:31 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Hyemin-Youn.netflix-clone.github.io/' // 실제 리포지토리 이름을 여기에 입력
+    : '/'
+});
+
