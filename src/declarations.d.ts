feee3f484a7442f3f3a157d9fc201f4bc5f938d3
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-07 01:58:39 +0900

    shims-vue.d.ts update

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,9 +1,3 @@
 // declarations.d.ts
 declare module '@/services/appwrite'; // 세미콜론 추가
 
-// shims-vue.d.ts
-declare module '*.vue' {
-    import { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-}
