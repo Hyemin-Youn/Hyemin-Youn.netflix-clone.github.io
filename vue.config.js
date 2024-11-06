c63e0f77452df53ccf99a0b210db745ea6aa0727
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-07 07:40:42 +0900

    update git router

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: (config) => {
-    // TypeScript 관련 플러그인을 제거합니다.
-    config.plugins = config.plugins.filter(
-      (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
-    );
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/hyemin-youn.github.io/' : '/'
 });
