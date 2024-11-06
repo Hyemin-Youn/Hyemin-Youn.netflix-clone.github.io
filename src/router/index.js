842ed7e83cfdb9b45c8782a89f3fe3afd6d6fb7b
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-07 05:07:45 +0900

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import SignIn from '@/views/SignIn.vue';
-import SignUp from '@/views/SignUp.vue';
-import HomePage from '@/views/Home.vue'; // 이름 변경
+import Home from '@/views/Home.vue';
+import LoginSignUp from '@/views/LoginSignUp.vue';
 
 const routes = [
-  { path: '/', redirect: '/signin' },
-  { path: '/signin', name: 'SignIn', component: SignIn },
-  { path: '/signup', name: 'SignUp', component: SignUp },
-  { path: '/home', name: 'HomePage', component: HomePage } // 이름 변경
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginSignUp
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: LoginSignUp
+  }
 ];
 
-
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
