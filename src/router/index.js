102ae74b350c5dd1398997d63502d0ad5fbb06c2
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-07 06:20:39 +0900

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
-import LoginSignUp from '@/views/LoginSignUp.vue';
+import LoginSignUp from '@/views/LoginSignUp.vue'; // 통합된 로그인/회원가입 컴포넌트
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/signin', // 기본 경로로 접속 시 /signin으로 리다이렉트
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: LoginSignUp
+    path: '/signin',
+    name: 'SignIn',
+    component: LoginSignUp, // 로그인/회원가입 컴포넌트
   },
   {
     path: '/signup',
-    name: 'Signup',
-    component: LoginSignUp
-  }
+    name: 'SignUp',
+    component: LoginSignUp, // 동일 컴포넌트 사용
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home, // 홈 컴포넌트
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
