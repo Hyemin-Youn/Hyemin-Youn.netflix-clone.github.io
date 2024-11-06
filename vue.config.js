4a6d3538dedbba3876cdf18cfd1ed02a3762cf64
Hyemin-Youn <hyemin9973@gmail.com>
2024-11-07 05:34:38 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,9 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: (config) => {
-    // TypeScript 관련 플러그인을 제거합니다.
     config.plugins = config.plugins.filter(
       (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin'
     );
   },
-  publicPath: '/',
+  publicPath: '/netflix-clone/', // 이 부분을 프로젝트 이름으로 설정
 });
